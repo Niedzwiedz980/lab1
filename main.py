f6088069b6e6d7d04bf20dd8f6fb487084de9c1c
Niedzwiedz980 <kuba123le@gmail.com>
2022-03-11 20:18:13 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+
+import math
+
+#zadanie1
+imie ="Jakub"
+nazwisko = "Jasiński"
+
+a=1
+b=2
+
+c=3.2
+d=4.3
+
+e=1+3j
+f=2+4j
+
+# print(imie)
+# print(nazwisko)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+#zadanie2
+i=6
+j=3
+# print(i+j)
+# print(i-j)
+# print(i*j)
+# print(i**j)
+# print(i/j)
+# print(i//j)
+# print(i%j)
+
+#zadanie3
+# x=10
+# print(x)
+# x+=1
+# print(x)
+# x-=1
+# print(x)
+# x*=2
+# print(x)
+# x/=2
+# print(x)
+# x**=2
+# print(x)
+# x%=2
+# print(x)
+
+#zadanie4
+
+# print(math.e**10)
+# print(math.pow(math.log(5+math.pow(math.sin(8),2),math.e),6))
+# print(math.floor(3.55))
+# print(math.ceil(4.80))
+
+#zadanie5
+
+imieWielkie = "JAKUB"
+nazwiskoWielkie ="JASIŃSKI"
+
+print(imieWielkie.capitalize()+nazwiskoWielkie.capitalize())
+
+#zadanie6
+
+# slowapiosenki = "la la la"
+# print(slowapiosenki.count("la"))
+
+#zadanie7
+
+# ciag = "To jest długi ciąg znaków"
+# print(ciag[0] + ciag[-1])
+# print(ciag[0] + ciag[len(ciag)-1])
+
+#zadanie8
+
+# nastepnapiosenka = "Ho Ho Ho"
+# slowa = nastepnapiosenka.split(' ')
+# print (slowa)
+
+#zadanie9
+
+znaki = "ciąg różńych znaków"
+liczba = 1.1
+kod = 0xDE
+print('To jest ciąg znaków: {0:s},to jest liczba zmiennoprzecinkowa: {1:.2f}, a to jest hex: {2:X}'.format(znaki,liczba,kod))
